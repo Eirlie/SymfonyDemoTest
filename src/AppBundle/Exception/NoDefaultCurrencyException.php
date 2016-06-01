7eab8386fe8bd8252044d59c2808866e5d68a6cd
Eirlie <eldar.shikhbadinov@gmail.com>
2016-06-01 15:55:11 +0300

    default currency added

---
+++ src/AppBundle/Exception/NoDefaultCurrencyException.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Date: 01.06.16
+ * Time: 14:33
+ */
+
+namespace AppBundle\Exception;
+
+/**
+ * Class NoDefaultCurrencyException
+ * @package AppBundle\Exception
+ * @author  Eldar Shikhbadinov <s.eldar@ideas-world.net>
+ */
+class NoDefaultCurrencyException extends \Exception
+{
+
+}
